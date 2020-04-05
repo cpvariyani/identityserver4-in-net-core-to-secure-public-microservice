d52f37beb8f7995368f59b066e20803db0094df0
CP Variyani <cpvariyani90@gmail.com>
2020-04-05 17:26:43 +0530

    Add files via upload

---
+++ IdentityServer_Config.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("myresourceapi", "My Resource API")
+                {
+                    Scopes = {new Scope("apiscope")}
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                // for public api
+                new Client
+                {
+                    ClientId = "secret_client_id",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "apiscope" }
+                }
+            };
+        }
+    }
+}
